244d6438ce12972549e9137bd41abac599cf94cf
escowin <edwin@escowinart.com>
2022-11-23 20:39:11 -0600

    rearranged & added details in portfolio array

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,85 @@
 const portfolio = [
     {
+        name: "Professional portfolio",
+        id: "professional-portfolio",
+        repo: "https://github.com/escowin/professional-portfolio",
+        liveUrl: "https://escowin.github.io/professional-portfolio",
+        languages: "html css js",
+        dialects: "",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Password generator",
+        id: "password-generator",
+        repo: "https://github.com/escowin/password-generator",
+        liveUrl: "https://escowin.github.io/password-generator/",
+        languages: "html css js es6",
+        dialects: "",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Coding quiz",
+        id: "coding-quiz",
+        repo: "https://github.com/escowin/coding-quiz",
+        liveUrl: "https://escowin.github.io/coding-quiz/",
+        languages: "html css js es6",
+        dialects: "",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Weather dashboard",
+        id: "weather-dashboard",
+        repo: "https://github.com/escowin/weather-dashboard",
+        liveUrl: "https://escowin.github.io/weather-dashboard/",
+        languages: "html css js es6 api",
+        dialects: "jquery moment",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Day planner",
+        id: "day-planner",
+        repo: "https://github.com/escowin/day-planner",
+        liveUrl: "https://escowin.github.io/day-planner/",
+        languages: "html css js",
+        dialects: "bootstrap jquery moment",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Team profile generator",
+        id: "team-profile-generator",
+        repo: "https://github.com/escowin/team-profile-generator",
+        liveUrl: null,
+        languages: "css js node es6",
+        dialects: "inquirer jest",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Portfolio generator",
+        id: "portfolio-generator",
+        repo: "https://github.com/escowin/portfolio-generator",
+        liveUrl: null,
+        languages: "css js node es6",
+        dialects: "inquirer",
+        highlight: false,
+        collaboration: false
+    },
+    {
+        name: "Readme generator",
+        id: "readme-generator",
+        repo: "https://github.com/escowin/readme-generator",
+        liveUrl: null,
+        languages: "js node es6",
+        dialects: "inquirer",
+        highlight: false,
+        collaboration: false
+    },
+    {
         name: "Escowin art",
         id: "escowinart",
         repo: "https://github.com/escowin/escowinart",
@@ -24,88 +104,8 @@ const portfolio = [
         id: "solar-weather",
         repo: "https://github.com/escowin/solar-weather-app",
         liveUrl: "https://escowin.github.io/solar-weather-app",
-        languages: "html css js api",
+        languages: "html css js es6 api",
         dialects: "jquery moment",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Team profile generator",
-        id: "team-profile-generator",
-        repo: "https://github.com/escowin/team-profile-generator",
-        liveUrl: null,
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Professional portfolio",
-        id: "professional-portfolio",
-        repo: "https://github.com/escowin/professional-portfolio",
-        liveUrl: null,
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Readme generator",
-        id: "readme-generator",
-        repo: "https://github.com/escowin/readme-generator",
-        liveUrl: "",
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Portfolio generator",
-        id: "portfolio-generator",
-        repo: "https://github.com/escowin/portfolio-generator",
-        liveUrl: "",
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Weather dashboard",
-        id: "weather-dashboard",
-        repo: "https://github.com/escowin/weather-dashboard",
-        liveUrl: "https://escowin.github.io/weather-dashboard/",
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Day planner",
-        id: "day-planner",
-        repo: "https://github.com/escowin/day-planner",
-        liveUrl: "https://escowin.github.io/day-planner/",
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Coding quiz",
-        id: "coding-quiz",
-        repo: "https://github.com/escowin/coding-quiz",
-        liveUrl: "https://escowin.github.io/coding-quiz/",
-        languages: "",
-        dialects: "",
-        highlight: false,
-        collaboration: false
-    },
-    {
-        name: "Password generator",
-        id: "password-generator",
-        repo: "https://github.com/escowin/password-generator",
-        liveUrl: "https://escowin.github.io/password-generator/",
-        languages: "",
-        dialects: "",
         highlight: false,
         collaboration: false
     },
@@ -114,8 +114,8 @@ const portfolio = [
         id: "smart-home",
         repo: "https://github.com/escowin/smart-home",
         liveUrl: "https://young-earth-27956.herokuapp.com/",
-        languages: "",
-        dialects: "",
+        languages: "mern stack",
+        dialects: "frontend lead",
         highlight: true,
         collaboration: true
     },
@@ -124,8 +124,8 @@ const portfolio = [
         id: "raven-home",
         repo: "https://github.com/escowin/raven-home",
         liveUrl: "https://escowin.github.io/RavenclawG-2/",
-        languages: "",
-        dialects: "",
+        languages: "html css js api",
+        dialects: "backend developer",
         highlight: false,
         collaboration: true
     },
@@ -134,8 +134,8 @@ const portfolio = [
         id: "auction-house",
         repo: "https://github.com/escowin/auction-house",
         liveUrl: "https://stormy-fjord-57144.herokuapp.com/",
-        languages: "",
-        dialects: "",
+        languages: "mvc interactive fullstack",
+        dialects: "frontend lead",
         highlight: false,
         collaboration: true
     },
