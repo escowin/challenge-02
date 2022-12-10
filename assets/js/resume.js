1633abc8a5b79758fa91f9a4747a4ea16a58c58f
escowin <edwin@escowinart.com>
2022-12-10 14:27:21 -0600

    display current year in resume footer

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -155,5 +155,16 @@ function generateExpertise(skillset) {
       displayEndeavors();
   }
   
+  function currentYear() {
+    let date = new Date().getFullYear();
+    const dateEl = document.querySelector("#date");
+    dateEl.textContent = date;
+  
+    console.log(`
+    \u00a9 ${date} Edwin M. Escobar
+    https://github.com/escowin/professional-portfolio`)
+  }
+
   // call
+  currentYear()
   displayResume();
